6178a0f72b5295eb4bf3f25fa7c2d86dcf67adf2
p51901229 <p51901229@brasilprev.com.br>
2022-04-13 09:24:47 -0300

    fix(crud-ajuste): correção de mecanica de crud

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,16 +1,16 @@
 from sqlalchemy.orm import Session
 import schema, models
 
-def save_file_cnab(db: Session, config: schema.CNAB):
-    config_model = models.CNAB(**config.dict())
-    db.add(config_model)
+def save_file_cnab(db: Session, config):
+    for line in config:
+        config_model = models.CNAB(**line)
+        db.add(config_model)
     db.commit()
     db.refresh(config_model)
     return config_model
 
 def get_file_cnab(db: Session):
-    print(db)
-    return db.query(models.CNAB).first()
+    return db.query(models.CNAB).all()
 
 def delete_file_cnab(db: Session):
     db.query(models.CNAB).delete()
