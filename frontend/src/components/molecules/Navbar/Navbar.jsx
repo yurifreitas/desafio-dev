e76f6f52e13342a83f0b4efd3181c3b3573afa3e
p51901229 <p51901229@brasilprev.com.br>
2022-04-13 10:56:38 -0300

    feature(): algumas melhorias

--- frontend/src/components/molecules/Navbar/Navbar.jsx
+++ frontend/src/components/molecules/Navbar/Navbar.jsx
@@ -12,30 +12,22 @@ const NavbarComponent = () => {
                     placement="end"
                 >
                     <Offcanvas.Header closeButton>
-                        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+                        <Offcanvas.Title id="offcanvasNavbarLabel">Upload Arquivos</Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
                         <Nav className="justify-content-end flex-grow-1 pe-3">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/arquivos">Arquivos</Nav.Link>
-                            <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+                            {/* <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
                                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action5">
                                     Something else here
                                 </NavDropdown.Item>
-                            </NavDropdown>
+                            </NavDropdown> */}
                         </Nav>
-                        <Form className="d-flex">
-                            <FormControl
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+                        
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
             </Container>
